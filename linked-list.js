d46f3fc67a8dc855c980f7d0123a5c359b36cf3a
Quinn Torres <quinntorrs@gmail.com>
2022-07-03 11:07:33 -0400

    Add documentation to linked-list.js

--- linked-list.js
+++ linked-list.js
@@ -4,6 +4,11 @@ class Node {
         this.next = null;
     }
 
+    /**
+     * Log the data in the node
+     *
+     * @return {undefined}
+     */
     toString() {
         return this.data;
     }
@@ -15,6 +20,13 @@ class LinkedList {
         this.size = 0;
     }
 
+    /**
+     * Add a new node to the linked list containing the given data
+     *
+     * @param data the data to put in the new node
+     *
+     * @return {undefined}
+     */
     push(data) {
         let node = new Node(data);
         node.next = this.head;
@@ -23,6 +35,11 @@ class LinkedList {
         this.size++;
     }
 
+    /**
+     * Remove and return the node at the beginning of the linked list
+     *
+     * @return {Node} the node at the beginning of the linked list
+     */
     pop() {
         let oldHead = this.head;
         this.head = oldHead.next;
@@ -32,6 +49,13 @@ class LinkedList {
         return oldHead;
     }
 
+    /**
+     * Add a new node to the end of the linked list with the given data
+     *
+     * @param data the data to add to the new node
+     *
+     * @return {undefined}
+     */
     add(data) {
         let currNode = this.head;
         let node = new Node(data);
@@ -43,6 +67,14 @@ class LinkedList {
         currNode.next = node;
     }
 
+    /**
+     * Add a new node to the linked list at the given index with the given data
+     *
+     * @param index the index to insert the new node at
+     * @param data the data to add to the new node
+     *
+     * @return {undefined}
+     */
     insertAt(index, data) {
         if (this.isInvalidIndex(index)) {
             return -1;
@@ -61,6 +93,13 @@ class LinkedList {
         }
     }
 
+    /**
+     * Remove a node from the linked list at the given index
+     *
+     * @param index the index of the node to remove
+     *
+     * @return {undefined}
+     */
     removeFrom(index) {
         if (this.isInvalidIndex(index)) {
             return -1;
@@ -74,6 +113,13 @@ class LinkedList {
         }
     }
 
+    /**
+     * Remove a node that matches the given data from the linked list 
+     *
+     * @param data the data to search the nodes for
+     *
+     * @return {undefined}
+     */
     remove(data) {
         let currNode = this.head;
 
@@ -90,6 +136,13 @@ class LinkedList {
         }
     }
 
+    /**
+     * Return the index of the node in the linked list that matches the given data, or -1 otherwise
+     *
+     * @param data the data to search for in the nodes
+     *
+     * @return {Number} the index of the matching node, or -1 otherwise
+     */
     indexOf(data) {
         let currNode = this.head;
         let index = 0;
@@ -106,10 +159,22 @@ class LinkedList {
         return -1;
     }
 
+    /**
+     * Return if the linked list is empty or not
+     *
+     * @return {Boolean} true if the linked list is empty
+     */
     isEmpty() {
         return this.size === 0;
     }
 
+    /**
+     * Return the node at a given index in the linked list, or -1 otherwise
+     *
+     * @param index the index of the node to return
+     *
+     * @return {Object} the node at the given index, or -1 otherwise
+     */
     get(index) {
         let currIndex = 0;
         let currNode = this.head;
@@ -130,10 +195,22 @@ class LinkedList {
         return currNode;
     }
 
+    /**
+     * Return if the given index of the linked list is outside of its valid range
+     *
+     * @param index the index to check the validity of
+     *
+     * @return {Boolean} true if the index is valid for the linked list
+     */
     isInvalidIndex(index) {
         return index < 0 || index >= this.size;
     }
 
+    /**
+     * Log the data in the linked list in order
+     *
+     * @return {undefined}
+     */
     toString() {
         let string = '[';
         let node = this.head;
@@ -153,41 +230,47 @@ class LinkedList {
 function test() {
     let list = new LinkedList();
 
+    // Push
     for(num of [...Array(10).keys()]) {
         list.push(num);
     }
 
+    // Initial Data
     console.log(`list: ${list}\n`);
 
-    console.log('push 10');
-    list.push(10);
-    console.log(`list: ${list}\n`);
-
+    // Pop
     console.log(`pop: ${list.pop()}`);
     console.log(`list: ${list}\n`);
 
+    // Add
     console.log(`add 11`);
     list.add(11);
     console.log(`list: ${list}\n`);
 
+    // Insert At
     console.log(`insertAt(3, 12)`);
     list.insertAt(3, 12);
     console.log(`list: ${list}\n`);
 
+    // Remove From
     console.log(`removeFrom(3)`);
     list.removeFrom(3);
     console.log(`list: ${list}\n`);
 
+    // Remove
     console.log(`remove(11)`);
     list.remove(11);
     console.log(`list: ${list}\n`);
 
+    // Index Of
     console.log(`indexOf(8): ${list.indexOf(8)}\n`);
 
     list = new LinkedList();
 
+    // Is Empty
     console.log(`isEmpty(): ${list.isEmpty()}\n`);
 
+    // Size
     console.log(`size: ${list.size}\n`);
 }
 
