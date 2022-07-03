6f861a1a54fbb7823f07b24ed52814ef55b14e56
Quinn Torres <quinntorrs@gmail.com>
2022-07-03 10:56:57 -0400

    Add documentation to heap.js

--- heap.js
+++ heap.js
@@ -15,16 +15,23 @@ class MaxHeap {
         this.heap.length = size + 1;
     }
 
+    /**
+     * Add a node to the heap according to the max heap rules
+     *
+     * @param node the node to add
+     *
+     * @return {undefined}
+     */
     insert(node) {
-        // add to end of heap
+        // Add to end of heap
         let length = this.heap.length;
         this.heap[length] = node;
 
-        // swim node up until its parent isn't bigger or doesn't exist
         let currIndex = length;
         let parentIndex = Math.floor(currIndex / 2);
         let parent = this.heap[parentIndex];
 
+        // Swim node up until its parent isn't bigger or doesn't exist
         while (parent && node > parent) {
             this.heap[parentIndex] = node;
             this.heap[currIndex] = parent;
@@ -35,15 +42,19 @@ class MaxHeap {
         }
     }
 
+    /**
+     * Remove and return the largest node in the heap
+     *
+     * @return {Number} the largest node in the heap
+     */
     max() {
         let result = this.heap[1];
         let node = this.heap[this.heap.length - 1];
 
-        // put last entry at the top and remove the last entry
+        // Put last entry at the top and remove the last entry
         this.heap[1] = node;
         this.heap.length--;
 
-        // sink down and swap with the largest of two children until the children aren't bigger or there are none
         let currIndex = 1;
         let leftIndex = currIndex * 2;
         let rightIndex = leftIndex + 1;
@@ -51,6 +62,7 @@ class MaxHeap {
         let leftChild = this.heap[leftIndex];
         let rightChild = this.heap[rightIndex];
 
+        // Sink down and swap with the largest of two children until the children aren't bigger or there are none
         while((leftChild || rightChild) && (leftChild > node || rightChild > node)) {
             if (!rightChild || (leftChild > rightChild)) {
                 this.heap[leftIndex] = node;
@@ -72,6 +84,11 @@ class MaxHeap {
         return result;
     }
 
+    /**
+     * Log all of the nodes in the heap, in the order they appear in the array
+     *
+     * @return {undefined}
+     */
     toString() {
         return this.heap;
     }
@@ -80,18 +97,23 @@ class MaxHeap {
 function test() {
     let heap = new MaxHeap();
 
+    // Insert
     for(num of [5, 2, 9, 6, 7, 1, 3, 8, 4]) {
         heap.insert(num);
     }
 
+    // Size
     console.log(`size: ${heap.size}\n`);
 
+    // Initial Data
     console.log(`heap: ${heap.toString()}\n`);
 
+    // Max
     console.log(`max(): ${heap.max()}`);
     console.log(`max(): ${heap.max()}`);
     console.log(`max(): ${heap.max()}`);
 
+    // toString
     console.log(`heap: ${heap.toString()}\n`);
 }
 
