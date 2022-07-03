60680ecf246135e8df96b060e023865fe4a55619
Quinn Torres <quinntorrs@gmail.com>
2022-07-03 11:12:25 -0400

    Add documentation to merge-sort.js

--- algorithms/merge-sort.js
+++ algorithms/merge-sort.js
@@ -1,25 +1,41 @@
+/**
+ * Sort an array of numbers using merge sort
+ *
+ * @param array the array to sort
+ *
+ * @return {Array} the array of sorted numbers
+ */
 function mergeSort(array) {
     return mergeSortRec(array, 0, array.length - 1);
 }
 
+/**
+ * Sort an array of numbers using merge sort, given a range of indexes to sort
+ *
+ * @param array the array to sort
+ * @param start the starting index to sort
+ * @param end the ending index to sort
+ *
+ * @return {Array} the array of sorted numbers within the given index
+ */
 function mergeSortRec(array, start, end) {
-    // base case - 1 element
+    // Base case: 1 element
     if (end <= start) {
         return [array[start]];
     }
 
-    // left half
+    // Merge sort left half
     let leftHalf = mergeSortRec(array, start, Math.floor((start + end) / 2));
 
-    // right half
+    // Merge sort right half
     let rightHalf = mergeSortRec(array, Math.floor((start + end) / 2) + 1, end);
 
-    // combine
     let leftIndex = rightIndex = 0;
     let currLeft = leftHalf[leftIndex];
     let currRight = rightHalf[rightIndex];
     let combined = [];
 
+    // Combine sorted halves
     while(currLeft || currRight) {
         if (!currLeft || currLeft > currRight) {
             combined.push(currRight);
@@ -32,14 +48,21 @@ function mergeSortRec(array, start, end) {
         }
     }
 
-    // return
+    // Return sorted array
     return combined;
 }
 
+/**
+ * Test the functionality of merge sort
+ *
+ * @return {undefined}
+ */
 function test() {
+    // Initial Data
     let array = [56, 12, 78, 34, 99, 2, 7, 1, 8, 48, 62, 72, 35, 89, 100];
     console.log(`array: ${array}`);
 
+    // Sorted Data
     let sortedArray = mergeSort(array);
     console.log(`sorted array: ${sortedArray}`);
 }
